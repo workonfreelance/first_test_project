141672f467792aa45f6b2fe8c50fcbfdac6520ba
niktarin <spainszlo@gmail.com>
2020-01-12 17:53:56 +0200

    updata

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.forms import forms, ModelForm
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tags(models.Model):
+    name = models.CharField(max_length=50)
+    img = models.ImageField(default="None",verbose_name="Картинка")
+
+    def __str__(self):
+        return self.name
+
+
+class Jobs(models.Model):
+    job_name = models.TextField(max_length=50,verbose_name="Название вакансии")
+    short_job_name = models.TextField(max_length=50,verbose_name="Краткое название вакансии")
+    tags = models.ManyToManyField(Tags,verbose_name="Теги")
+    loacation = models.TextField(max_length=50,verbose_name="Местонахождение")
+    hot = models.BooleanField(default=False,verbose_name="Hot")
+    body = models.TextField(default="",verbose_name="Описание")
+
+    class Meta:
+        ordering = ('hot',)
+
+
+
+
